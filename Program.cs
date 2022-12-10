408875226431b07ca43f2a0d93d3d3233fae5a30
gnalvesteffer <xorberax@gmail.com>
2022-12-10 12:24:43 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.Write("Enter file path to calculate MD5 hash for: ");
+Console.WriteLine(CalculateMd5HashForFile(Console.ReadLine().Replace("\"", string.Empty).Trim()));
+
+string? CalculateMd5HashForFile(string filePath)
+{
+    if (!File.Exists(filePath))
+    {
+        return null;
+    }
+
+    using (var md5 = MD5.Create())
+    {
+        using (var stream = File.OpenRead(filePath))
+        {
+            return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLowerInvariant();
+        }
+    }
+}
